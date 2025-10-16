1f14dcf93d324813b89ab64d4d885d725ab438f4
anchel.silva <anchel.varela@gmail.com>
2025-10-16 19:32:00 -0300

    implementa endpoint para busca de pokemons

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask_jwt_extended import JWTManager
 from app.db import db
 from .routes.auth import auth_bp
+from .routes.pokemon import pokemon_bp
 
 def create_app():
     app = Flask(__name__)
@@ -14,6 +15,7 @@ def create_app():
     jwt = JWTManager(app)
 
     app.register_blueprint(auth_bp, url_prefix="/auth")
+    app.register_blueprint(pokemon_bp, url_prefix="/pokemon")
 
     @app.route("/")
     def index():
---
+++ app/routes/pokemon.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, request, jsonify
+import requests
+
+pokemon_bp = Blueprint("pokemon", __name__)
+POKEAPI_BASE_URL = "https://pokeapi.co/api/v2/pokemon"
+
+@pokemon_bp.route("/", methods=['GET'])
+def listarPokemons():
+    response = requests.get(f"{POKEAPI_BASE_URL}")
+
+    if response.status_code != 200:
+        return jsonify({"error": "Falha ao acessar a PokeAPI"}), 500
+    
+    data = response.json()
+    resultados = []
+
+    for item in data['results']:
+        nome = item['name']
+        detalhes = requests.get(item['url'])
+
+        if detalhes.status_code != 200:
+            continue
+
+        detalhes = detalhes.json()
+
+        codigo = detalhes['id']
+        imagem_url = detalhes['sprites']['front_default']
+        tipos = [t['type']['name'] for t in detalhes['types']]
+
+        resultados.append({
+            "Codigo": codigo,
+            "Nome": nome,
+            "ImagemUrl": imagem_url,
+            "Tipos": tipos
+        })
+
+    return jsonify(resultados)
